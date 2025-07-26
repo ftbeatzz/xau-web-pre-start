d7bf6d15af8bbec9db086ba7357821af8434d3f2
ftbeat <downgabe@list.ru>
2025-07-26 18:02:09 +0300

    modalResponsive2.0

--- src/main.tsx
+++ src/main.tsx
@@ -4,21 +4,8 @@ import Router from './Router'
 import './index.css'
 import './i18n'
 
-// Функция для установки корректной высоты на мобильных устройствах
-const setVH = () => {
-	const vh = window.innerHeight * 0.01
-	document.documentElement.style.setProperty('--vh', `${vh}px`)
-}
-
-// Инициализируем высоту при загрузке
-setVH()
-
-// Обновляем высоту при изменении размера окна
-window.addEventListener('resize', setVH)
-window.addEventListener('orientationchange', setVH)
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<Router />
+			<Router />
 	</React.StrictMode>
 )
